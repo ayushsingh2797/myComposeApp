2668f6dece65ee952dd96f62883d62c1676c9ff3
Ayush Singh <ayushsingh2797@gmail.com>
2025-04-20 01:13:06 +0530

    New Compose functions added

--- app/src/main/java/com/example/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -4,22 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,10 +77,9 @@ fun ShowMyButton() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun ShowEditText(){
-    val state  = remember { mutableStateOf("") }
+fun ShowEditText() {
+    val state = remember { mutableStateOf("") }
     TextField(
         value = state.value,
         onValueChange = {
@@ -79,4 +90,63 @@ fun ShowEditText(){
         },
         maxLines = 1
     )
+}
+
+@Composable
+fun DefaultPreviewFunction() {
+    /*Column {
+        Text(text = "A", fontSize = 20.sp)
+        Text(text = "B", fontSize = 20.sp)
+    }*/
+
+    /*Row {
+        Text(text = "A", fontSize = 20.sp)
+        Text(text = "B", fontSize = 20.sp)
+    }*/
+
+    Box(contentAlignment = Alignment.Center) {
+        Image(painterResource(id = R.drawable.heart_image), contentDescription = "Heart Image")
+        //Image(painterResource(id = R.drawable.arrows_png),contentDescription = "Arrow Image")
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewFunction() {
+    TextModifier()
+}
+
+
+@Composable
+fun ListViewItem(imageId:Int,name: String,occupation: String) {
+    Row {
+        Image(
+            painterResource(id = imageId),
+            contentDescription = "Profile Image",
+            Modifier.size(50.dp)
+        )
+
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = name, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Text(text = occupation, fontSize = 15.sp, fontWeight = FontWeight.Thin)
+        }
+    }
+}
+
+@Composable
+fun TextModifier(){
+    Text(
+        text = "Hello Ayush",
+        color = Color.Black,
+        modifier = Modifier.background(Color.Blue)
+            .size(200.dp)
+            .border(4.dp,Color.Red)
+            .padding(30.dp)
+            .clip(RectangleShape)
+            .background(Color.Yellow)
+        )
 }
